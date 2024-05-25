a158c4bc8c1d5cc1e39705a909cc578cb7846f8f
Shyngys Aitkazinov <chikaphys9@gmail.com>
2024-05-25 17:03:21 +0200

    perception: xor and eye gaze tutorials

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="perception",
+    version="1.0.0",
+    author="Shyngys Aitkazinov",
+    author_email="chikaphys9@gmail.com",
+    description="perception package, mostly containing deep learing models for computer vision tasks",
+    packages=find_packages(),
+)
