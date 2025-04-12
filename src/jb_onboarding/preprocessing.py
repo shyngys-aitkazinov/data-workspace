0c2bcf29a777a24841884e00e9d472a8efeb4c78
Shyngys Aitkazinov <chikaphys9@gmail.com>
2025-04-12 16:31:38 +0200

    preproc

--- src/jb_onboarding/preprocessing.py
+++ src/jb_onboarding/preprocessing.py
@@ -1,5 +1,10 @@
 import os
+import zipfile
 from os import PathLike
+
+from docx import Document
+
+from jb_onboarding.constants import DOCS
 
 
 class Preprocessor:
@@ -10,8 +15,22 @@ class Preprocessor:
         """
         pass
 
-    def __call__(self, path_to_zip: PathLike, path_to_data: PathLike, **kwargs):
+    def __call__(self, path_to_zip: PathLike):
         """
         This method is called when the Preprocessor class is instantiated.
         It initializes the class with the provided arguments.
         """
+        with zipfile.ZipFile(path_to_zip, "r") as zip_ref:
+            # extract the zip file to a temporary directory
+            temp_dir = str(path_to_zip).replace(".zip", "")
+            zip_ref.extractall(temp_dir)
+            # check if the profile.docx file exists in the zip archive
+            if "profile.docx" not in zip_ref.namelist():
+                raise FileNotFoundError("profile.docx not found in the zip archive.")
+
+            with zip_ref.open("profile.docx") as docx_file:
+                profile = Document(docx_file)
+
+            if "passport.png" not in zip_ref.namelist():
+                raise FileNotFoundError("passport.png not found in the zip archive.")
+            passport_path = os.path.join(temp_dir, "passport.png")
