45922e920c04004624955c588cb83ded815acd46
Shyngys Aitkazinov <chikaphys9@gmail.com>
2025-04-12 13:55:19 +0200

    preproc

---
+++ src/jb_onboarding/preprocessing.py
@@ -0,0 +1,15 @@
+class Preprocessor:
+    def __init__(self, *args, **kwargs):
+        """
+        This is the constructor method for the Preprocessor class.
+        It initializes the class with the provided arguments.
+        """
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, *args, **kwargs):
+        """
+        This method is called when the Preprocessor class is instantiated.
+        It initializes the class with the provided arguments.
+        """
+        pass
